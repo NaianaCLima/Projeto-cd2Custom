ff9f6bf8f104a1bbf9b5c5d9637a4f0698b53552
NaianaCLima <naianacordeirodelima@gmail.com>
2022-02-05 17:04:04 -0300

    Projeto backend criado

---
+++ src/main/java/com/cd2Custom/Transporte/SigaBem/TransporteSigaBemApplication.java
@@ -0,0 +1,13 @@
+package com.cd2Custom.Transporte.SigaBem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransporteSigaBemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransporteSigaBemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cd2Custom/Transporte/SigaBem/TransporteSigaBemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cd2Custom.Transporte.SigaBem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransporteSigaBemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
